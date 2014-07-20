850cc515650432cff6c532defd5bf2498a6907f1
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@6e4a4417-3737-8b9a-f2c9-f195e6692d1b>
2014-07-20 19:14:22 +0000

    Modyfikacja licznika online przez REST, logowanie, budowanie executable jar na Heroku

--- mirkoonline-bot/src/main/java/org/grizz/web/UsersOnlineController.java
+++ mirkoonline-bot/src/main/java/org/grizz/web/UsersOnlineController.java
@@ -1,25 +1,34 @@
 package org.grizz.web;
 
 import org.grizz.model.UserCounter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by Grizz on 2014-07-19.
  */
 @RestController
 public class UsersOnlineController {
+    Logger log = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private UserCounter userCounter;
 
     @RequestMapping(value = "/", method = GET)
-    public UserCounter home() {
+    public UserCounter getUserCount() {
         return userCounter;
+    }
+
+    @RequestMapping(value = "/setNewCounter/{count}", method = GET)
+    public boolean saveUserCount(@PathVariable("count") final int count) {
+        log.info("Setting new counter to " + count);
+        this.userCounter.setCount(count);
+        return true;
     }
 }
