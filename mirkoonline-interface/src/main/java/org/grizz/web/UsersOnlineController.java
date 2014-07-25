7a002ba729ee6106d05cf859a33e57223c06bfd4
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@6e4a4417-3737-8b9a-f2c9-f195e6692d1b>
2014-07-25 21:07:25 +0000

    Wsparcie dla jsonp

--- mirkoonline-interface/src/main/java/org/grizz/web/UsersOnlineController.java
+++ mirkoonline-interface/src/main/java/org/grizz/web/UsersOnlineController.java
@@ -1,11 +1,16 @@
 package org.grizz.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.grizz.model.UserCounter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -30,5 +35,14 @@ public class UsersOnlineController {
         log.info("Setting new counter to " + count);
         this.userCounter.setCount(count);
         return true;
+    }
+
+    @RequestMapping(value = "/jsonp", method = RequestMethod.GET)
+    @ResponseBody
+    public String jsonp(@RequestParam("c")String callBack) throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("counter", userCounter.getCount());
+        return objectMapper.writeValueAsString(new JSONPObject(callBack,map));
     }
 }
