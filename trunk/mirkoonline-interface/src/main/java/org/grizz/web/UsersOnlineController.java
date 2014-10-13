5252e1f16a51bd6a52e609db39e752e83501ea54
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@6e4a4417-3737-8b9a-f2c9-f195e6692d1b>
2014-10-13 18:43:19 +0000

    data aktualizacji licznika, historia licznikow, przekierowanie na domene mirkoonline.grizwold.pl

--- trunk/mirkoonline-interface/src/main/java/org/grizz/web/UsersOnlineController.java
+++ trunk/mirkoonline-interface/src/main/java/org/grizz/web/UsersOnlineController.java
@@ -6,9 +6,20 @@ import org.grizz.model.UserCounter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,11 +34,13 @@ public class UsersOnlineController {
     Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
+    private ApplicationContext ctx;
+    @Autowired
     private UserCounter userCounter;
 
-    @RequestMapping(value = "/", method = GET)
-    public UserCounter getUserCount() {
-        return userCounter;
+    @RequestMapping(value = "/", method = GET, produces = MediaType.TEXT_HTML_VALUE)
+    public RedirectView getUserCount() throws IOException {
+        return new RedirectView("http://mirkoonline.grizwold.pl/");
     }
 
     @RequestMapping(value = "/setNewCounter/{count}", method = GET)
@@ -37,12 +50,22 @@ public class UsersOnlineController {
         return true;
     }
 
-    @RequestMapping(value = "/jsonp", method = RequestMethod.GET)
+    @RequestMapping(value = "/jsonp", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String jsonp(@RequestParam("callback")String callBack) throws Exception{
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("counter", userCounter.getCount());
+        map.put("updated", userCounter.getUpdated());
         return objectMapper.writeValueAsString(new JSONPObject(callBack,map));
+    }
+
+    @RequestMapping(value = "/history/jsonp", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String history(@RequestParam("callback")String callBack) throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("history", userCounter.getHistory());
+        return objectMapper.writeValueAsString(new JSONPObject(callBack, userCounter.getHistory()));
     }
 }
