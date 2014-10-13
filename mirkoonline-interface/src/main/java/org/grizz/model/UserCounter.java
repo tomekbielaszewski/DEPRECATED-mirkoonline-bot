786f1b98ee48720efd2ef3707c36948c64c7ae69
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@6e4a4417-3737-8b9a-f2c9-f195e6692d1b>
2014-10-13 18:43:19 +0000

    data aktualizacji licznika, historia licznikow, przekierowanie na domene mirkoonline.grizwold.pl

--- mirkoonline-interface/src/main/java/org/grizz/model/UserCounter.java
+++ mirkoonline-interface/src/main/java/org/grizz/model/UserCounter.java
@@ -1,6 +1,10 @@
 package org.grizz.model;
 
+import com.google.common.collect.Maps;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by Grizz on 2014-07-19.
@@ -8,12 +12,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserCounter {
     private int count;
+    private Date updated;
+    private Map<Long, Integer> history = Maps.newHashMap();
 
     public int getCount() {
         return count;
     }
 
+    public Date getUpdated() {
+        return updated;
+    }
+
     public void setCount(int count) {
+        if(this.updated != null) {
+            history.put(this.updated.getTime(), this.count);
+        }
+
         this.count = count;
+        this.updated = new Date();
+    }
+
+    public Map<Long, Integer> getHistory() {
+        return history;
     }
 }
